adde72825219452daee237cc241a6646ebf1d768
Kamil Stangret <kamilstangret@wp.pl>
2018-04-23 20:49:49 +0200

    Add all files with basic content

---
+++ calculateTotalPoints.js
@@ -0,0 +1,11 @@
+const calculateDistancePoints = require('./calculateDistancePoints');
+const calculateStyleNotes = require('./calculateStyleNotes');
+
+const calculateTotalPoints = (distance, hillSize, kPoint, styleNotes, windFactor, gateFactor) => {
+  const distancePoints = calculateDistancePoints(distance, hillSize, kPoint);
+  const stylePoints = calculateStyleNotes(styleNotes);
+
+  return distancePoints + stylePoints + windFactor + gateFactor;
+};
+
+module.exports = calculateTotalPoints;
