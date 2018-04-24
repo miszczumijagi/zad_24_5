72df30589599ad8dc6aad421d21cc09522697f0c
Kamil Stangret <kamilstangret@wp.pl>
2018-04-24 22:16:15 +0200

    Add calculating functions of distance and style points

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,5 +1,39 @@
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
-  ...
+
+	const normalHill = {
+		startPoints: 60,
+		meterValue: 2
+	};
+
+	const bigHill = {
+		startPoints: 60,
+		meterValue: 1.8
+	};
+
+	const giantHill = {
+		startPoints: 120,
+		meterValue: 1.2
+	};
+
+	function getDistancePoints(hill) {
+    if (distance == kPoint) {
+      return hill.startPoints;
+    } else {
+      let finalPoints = (distance - kPoint) * hill.meterValue + hill.startPoints;
+      return finalPoints;
+    }
+  }
+
+  switch (hillSize) {
+    case 'giant':
+      return getDistancePoints(giantHill);
+    case 'big':
+      return getDistancePoints(bigHill);
+    case 'normal':
+      return getDistancePoints(normalHill);
+    default:
+      return `Error: Wrong value given. Size of hill choose from: 'giant', 'big', 'normal'`;
+  }
 };
 
 module.exports = calculateDistancePoints;
--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,5 +1,37 @@
-const calculateStylePoints = (styleNotes) => {
-  ...
+const calculateStyleNotes = (styleNotes) => {
+
+	let arraySorted,
+			styleNotesSum;
+
+	function compareNumbers(a, b) {
+		return a - b;
+	}
+
+	function integerToFloat(a) {
+		let string = a.toFixed(1);
+		return Number(string);
+	}
+
+	function getSum(a, b) {
+		return a + b;
+	}
+
+	if (styleNotes.some(isNaN)) {
+
+		return `Error: Wrong value passed. Check if array of notes consists solely of numbers`;
+
+	} else {
+	
+	arraySorted = styleNotes.sort(compareNumbers);
+	arraySorted.pop();
+	arraySorted.shift();
+	arraySorted = arraySorted.map(integerToFloat);
+
+	styleNotesSum = arraySorted.reduce(getSum);
+
+	return styleNotesSum;
+
+	}
 };
 
-module.exports = calculateStylePoints;
+module.exports = calculateStyleNotes;
